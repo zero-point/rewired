41f1b95600def5192aa147ddb2efb9ba4c4ef652
David Jones <david.jones@fmail.co.uk>
2014-03-16 14:02:37 +0000

    Use a (slightly) more consistent JS coding style
    * Not necessarily more logical or useful
    * <http://contribute.jquery.org/style-guide/js/> is half right

--- script.js
+++ script.js
@@ -1,52 +1,53 @@
- var directionsDisplay;
- var google, directionsService = new google.maps.DirectionsService();
- var map;
+var directionsDisplay, google, map,
+	directionsService = new google.maps.DirectionsService();
 
- function initialize()
- {
-   directionsDisplay = new google.maps.DirectionsRenderer();
-   var loc = new google.maps.LatLng(55.8580, -4.2590);
-   var myOptions = {
-	 zoom:12,
-	 mapTypeId: google.maps.MapTypeId.ROADMAP,
-	 center: loc
-   };
+function
+initialize()
+{
+	var loc = new google.maps.LatLng( 55.8580, -4.2590 ),
+		myOptions = {
+			zoom:12,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			center: loc
+		};
 
-   map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-   directionsDisplay.setMap(map);
+	directionsDisplay = new google.maps.DirectionsRenderer();
+	map = new google.maps.Map( document.getElementById( 'map_canvas' ), myOptions );
+	directionsDisplay.setMap( map );
 
-   document.getElementById("select_route").onclick = function(event) {
-	 event.preventDefault();
-	 calcRoute();
-   };
- }
+	document.getElementById( 'select_route' ).onclick = function ( event ) {
+		event.preventDefault();
+		calcRoute();
+	};
+}
 
-function calcRoute()
- {
-   var start = document.getElementById("start").value;
-   var end = document.getElementById("end").value;
-   var distanceInput = document.getElementById("distance");
-   var footprintInput = document.getElementById("footprint");
-   var methodTransport = document.getElementById("method").value;
-   var request = {
-	 origin:start,
-	 destination:end,
-	 travelMode: google.maps.DirectionsTravelMode[methodTransport]
-   };
+function
+calcRoute()
+{
+	var start = document.getElementById( 'start' ).value,
+		end = document.getElementById( 'end' ).value,
+		distanceInput = document.getElementById( 'distance' ),
+		footprintInput = document.getElementById( 'footprint' ),
+		methodTransport = document.getElementById( 'method' ).value,
+		request = {
+			origin: start,
+			destination: end,
+			travelMode: google.maps.DirectionsTravelMode[methodTransport]
+		};
 
-   directionsService.route(request, function(response, status) {
-	 if (status == google.maps.DirectionsStatus.OK) {
-	   directionsDisplay.setDirections(response);
-	   distanceInput.value = response.routes[0].legs[0].distance.value / 1000;
-	   if (methodTransport == "DRIVING") {
-		 footprintInput.value = distanceInput.value * 430 * 0.62137;
-	   } else if (methodTransport == "TRANSIT") {
-	   footprintInput.value = distanceInput.value * 89;
-	   } else {
-		 footprintInput.value = 0;
-	   }
-	 }
-   });
- }
+	directionsService.route( request, function ( response, status ) {
+		if ( status == google.maps.DirectionsStatus.OK ) {
+			directionsDisplay.setDirections( response );
+			distanceInput.value = response.routes[0].legs[0].distance.value / 1000;
+			if ( methodTransport == 'DRIVING' ) {
+				footprintInput.value = distanceInput.value * 430 * 0.62137;
+			} else if ( methodTransport == 'TRANSIT' ) {
+				footprintInput.value = distanceInput.value * 89;
+			} else {
+				footprintInput.value = 0;
+			}
+		}
+	});
+}
 
- window.onload = initialize;
+window.onload = initialize;
